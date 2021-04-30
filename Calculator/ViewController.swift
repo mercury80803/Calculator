0a7872e90b1b71a4b9f5efe11a6f81fef480af0f
Zeki Bankat <zeki.bankat@me.com>
2021-04-30 13:56:37 +0300

    vbv

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func topla(_ sender: Any) {
+        
+        
+    }
+    @IBOutlet weak var sonuc: UILabel!
+    @IBOutlet weak var bolme: UIButton!
+    @IBOutlet weak var carpma: UIButton!
+    @IBOutlet weak var cikarma: UIButton!
+    @IBOutlet weak var toplama: UIButton!
+    @IBOutlet weak var txtbox2: UITextField!
+    @IBOutlet weak var Txtbox: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+       
+        
     }
 
-
+    
 }
 
